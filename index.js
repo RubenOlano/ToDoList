3f9adc61285e5f68d5c797b4d8b17fcce0248560
Ruben Olano <rubendavid2711@hotmail.com>
2021-10-08 15:33:52 -0500

    add date module and refactor code in index.js remove label around checkbox because it removed the strikethrough when checked

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const date = require(__dirname + '/date.js')
 
 const app = express()
 
@@ -7,8 +8,8 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"))
 
-let items = ["Buy Food", "Cook Food", "Eat Food"]
-let workItems = []
+const items = ["Buy Food", "Cook Food", "Eat Food"]
+const workItems = []
 
 app.get('/about', function (req, res){
     res.render("about")
@@ -18,27 +19,13 @@ app.get('/work', function (req, res){
     res.render("list", {listTitle: 'Work List', itemList: workItems})
 })
 
-app.post('/work', function (req, res){
-    let item = req.body.newItem
-    workItems.push(item)
-    res.redirect('/work')
-})
-
 app.get('/', function (req, res){
-    let today = new Date()
-    let options = {
-        weekday: 'long',
-        day: 'numeric',
-        month: 'long'
-    };
-    let day = today.toLocaleDateString("en-us", options)
-
-    res.render('list', {listTitle: day, itemList: items })
+    res.render('list', {listTitle: date.getDate(), itemList: items })
 
 })
 
 app.post('/', function (req, res){
-    let item = req.body.newItem
+    const item = req.body.newItem
 
     if (req.body.list === "Work") {
         workItems.push(item)
