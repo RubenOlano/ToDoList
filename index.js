1389d2eccecf52cfa7639fa2d72c9ec08faeaebc
Ruben Olano <rubendavid2711@hotmail.com>
2021-10-08 15:09:55 -0500

    add work list and separate both lists

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"))
 
 let items = ["Buy Food", "Cook Food", "Eat Food"]
+let workItems = []
+
+app.get('/work', function (req, res){
+    res.render("list", {listTitle: 'Work List', itemList: workItems})
+})
+
+app.post('/work', function (req, res){
+    let item = req.body.newItem
+    workItems.push(item)
+    res.redirect('/work')
+})
 
 app.get('/', function (req, res){
     let today = new Date()
@@ -18,14 +29,21 @@ app.get('/', function (req, res){
     };
     let day = today.toLocaleDateString("en-us", options)
 
-    res.render('list', {dayOfWeek: day, itemList: items })
+    res.render('list', {listTitle: day, itemList: items })
 
 })
 
 app.post('/', function (req, res){
     let item = req.body.newItem
-    items.push(item)
-    res.redirect("/")
+
+    if (req.body.list === "Work") {
+        workItems.push(item)
+        res.redirect('/work')
+    }
+    else {
+        items.push(item)
+        res.redirect("/")
+    }
 })
 
 app.listen(3000, function (){
